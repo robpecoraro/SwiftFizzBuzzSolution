3e6dae572ea2ea7789877352153996c88c11bbdf
Rob Pecoraro <rfpecoraro@gmail.com>
2018-09-16 21:05:56 -0400

    Create fizzBuzz.swift

---
+++ fizzBuzz.swift
@@ -0,0 +1,11 @@
+for i in 1...100 {
+    if (i%3 + i%5) == 0 {
+        print("fizzbuzz")
+    } else if i%3 == 0 {
+        print("fizz")
+    } else if i%5 == 0{
+        print("buzz")
+    } else {
+        print(i)
+    }
+}
